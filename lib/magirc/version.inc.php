76a4ead4c7022e61d117c4804affe61b6d1fe7f0
Sebastian Vassiliou <hal9000@denorastats.org>
2009-05-30 16:50:21 +0000

    ok, everything is ready to implement setup and admin parts...

--- lib/magirc/version.inc.php
+++ lib/magirc/version.inc.php
@@ -4,7 +4,7 @@
 define('VERSION_MAJOR', '0');
 define('VERSION_MINOR', '0');
 define('VERSION_REVISION', '0');
-define('VERSION_BUILD', '10');
+define('VERSION_BUILD', '11');
 define('VERSION_EXTRA', '-DEV');
 
 if (DEBUG) {
