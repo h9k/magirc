f1b56a0c8c88e30cad7cba935d682fcbd59b6640
Sebastian Vassiliou <hal9000@denorastats.org>
2009-06-02 19:04:00 +0000

    fixes and improvements to server class

--- inc/server.inc.php
+++ inc/server.inc.php
@@ -1,10 +1,13 @@
 <?php
 // $Id: status.inc.php 34 2009-06-01 11:10:30Z hal9000 $
 
-$server = new Server($_GET['server']);
-
-$magirc->tpl->assign('serverlist', $magirc->denora->getServers());
-$magirc->tpl->assign('server', $server);
-$magirc->tpl->display('server.tpl');
+if (isset($_GET['server'])) {
+	$server = new Server($_GET['server']);
+	$magirc->tpl->assign('server', $server);
+	$magirc->tpl->display('server_details.tpl');
+} else {
+	$magirc->tpl->assign('serverlist', $magirc->denora->getServers());
+	$magirc->tpl->display('server.tpl');
+}
 
 ?>
