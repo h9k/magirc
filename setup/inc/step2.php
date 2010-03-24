640c875ed406cd1ea559b5c5942c24b7b31c52c7
Sebastian Vassiliou <hal9000@denorastats.org>
2010-03-24 19:55:34 +0000

    fixed setting wrong base_url parameter during setup

--- setup/inc/step2.php
+++ setup/inc/step2.php
@@ -4,12 +4,13 @@
 $config['debug_mode'] = 0;
 $config['show_exec_time'] = 0;
 $error = 0;
-
+    
 $check = $setup->configCheck();
 $setup->tpl->assign('check', $check);
 if (!$check) { // Dump file to db
     $result = $setup->configDump();
-    $base_url = explode('setup/', $_SERVER['PHP_SELF']);
+    $protocol = @$_SERVER['HTTPS'] ? 'https://' : 'http://';
+    $base_url = $protocol.$_SERVER['SERVER_NAME'].str_replace('setup/index.php', '', $_SERVER['PHP_SELF']);
     $query = sprintf("UPDATE `magirc_config` SET `value` = '%s' WHERE `parameter` = 'base_url'", $base_url[0]);
     $setup->db->query($query, SQL_NONE);
     $setup->tpl->assign('result', $result);
