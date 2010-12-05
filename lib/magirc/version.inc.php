24d912ee4928711ce26db3ea71c04fe40b389939
Sebastian Vassiliou <hal9000@denorastats.org>
2010-12-05 22:37:09 +0000

    * added missing directories and preliminary instructions for localization

--- lib/magirc/version.inc.php
+++ lib/magirc/version.inc.php
@@ -4,7 +4,7 @@
 define('VERSION_MAJOR', '0');
 define('VERSION_MINOR', '6');
 define('VERSION_REVISION', '0');
-define('VERSION_BUILD', '93');
+define('VERSION_BUILD', '94');
 define('VERSION_EXTRA', '-DEV');
 
 define('VERSION_FULL', sprintf('%s.%s.%s.%s%s', VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD, VERSION_EXTRA));
