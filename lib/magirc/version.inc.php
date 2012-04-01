b9abf9041655e7884b140e14c966ca5491117191
Hal9000 <hal9000@denorastats.org>
2012-04-02 01:36:47 +0200

    bumped version to 0.7.2 for development

--- lib/magirc/version.inc.php
+++ lib/magirc/version.inc.php
@@ -2,7 +2,7 @@
 
 define('VERSION_MAJOR', '0');
 define('VERSION_MINOR', '7');
-define('VERSION_REVISION', '1');
+define('VERSION_REVISION', '2');
 define('VERSION_EXTRA', '-alpha');
 
 define('VERSION_FULL', sprintf('%s.%s.%s%s', VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_EXTRA));
