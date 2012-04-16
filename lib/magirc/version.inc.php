8fdfc212f7ec2d0e7ac55d8114a11061a423c6a6
Hal9000 <hal9000@denorastats.org>
2012-04-16 21:04:59 +0200

    Bumped version to v0.8.0-beta since this will be the next release

--- lib/magirc/version.inc.php
+++ lib/magirc/version.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 define('VERSION_MAJOR', '0');
-define('VERSION_MINOR', '7');
-define('VERSION_REVISION', '3');
-define('VERSION_EXTRA', '-alpha');
+define('VERSION_MINOR', '8');
+define('VERSION_REVISION', '0');
+define('VERSION_EXTRA', '-beta');
 
 define('VERSION_FULL', sprintf('%s.%s.%s%s', VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_EXTRA));
 
