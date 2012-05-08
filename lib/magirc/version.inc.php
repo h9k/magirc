b8dc62c16bdea895b9512746a7331a07b0e3896a
Hal9000 <hal9000@denorastats.org>
2012-05-08 15:47:20 +0200

    make htaccess check for mod_filter for AddOutputFilterByType

--- lib/magirc/version.inc.php
+++ lib/magirc/version.inc.php
@@ -2,7 +2,7 @@
 
 define('VERSION_MAJOR', '0');
 define('VERSION_MINOR', '8');
-define('VERSION_REVISION', '2');
+define('VERSION_REVISION', '3');
 define('VERSION_EXTRA', '-beta');
 
 define('VERSION_FULL', sprintf('%s.%s.%s%s', VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_EXTRA));
